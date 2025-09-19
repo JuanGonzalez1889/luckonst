be0aaf3acf04831b7770ea8a4c7f7d2d79520597
Juan Gonzalez <juanpablogonzalez94@hotmail.com>
2025-09-19 17:36:56 -0300

    sacando boton proyectos realizados mobile

--- components/header.tsx
+++ components/header.tsx
@@ -190,7 +190,7 @@ export function Header() {
                 Contacto
               </button>
               <div className="mx-4">
-                <Link href="/proyectos" onClick={() => setIsMobileMenuOpen(false)}>
+                {/* <Link href="/proyectos" onClick={() => setIsMobileMenuOpen(false)}>
                   <Button className="w-full bg-primary hover:bg-primary/90 text-white font-medium rounded-full px-4 py-2 flex items-center justify-center gap-2">
                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path
@@ -202,7 +202,7 @@ export function Header() {
                     </svg>
                     Proyectos realizados
                   </Button>
-                </Link>
+                </Link> */}
               </div>
               <div className="flex justify-center space-x-4 pt-4 border-t border-gray-100 mx-4">
                 <a
